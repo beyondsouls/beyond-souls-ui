2fdaf016373ab91a6b129e14602b67d88e3b113a
Jan Biasi <biasijan@gmail.com>
2017-09-07 01:37:39 +0200

    Push enhanced js delegate logic for atuomated events

--- src/factory.js
+++ src/factory.js
@@ -11,13 +11,27 @@ export function addEvent(el, eventType, handler) {
     }
 }
 
+export const classListToSelector = classes => `.${classes.split(" ").join(".")}`;
+
 export default (ComponentClass, componentInstance, node) => {
     if (typeof componentInstance.on === "function") {
         const events = componentInstance.events || ComponentClass.events;
-        if (Array.isArray(events)) {
+        const delegate = node.getAttribute("data-bynd-delegate");
+
+        if (delegate) {
+            node = node.querySelector(delegate);
+            if (node) {
+                node.setAttribute("data-bynd-parent-ev-ref", classListToSelector(node.className));
+            }
+        }
+
+        if (node && Array.isArray(events)) {
+            console.log(node);
             events.forEach(eventType => {
                 addEvent(node, eventType, ev => {
-                    componentInstance.on(eventType, ev);
+                    componentInstance.on(eventType, ev, {
+                        delegate: !!delegate
+                    });
                 });
             });
         }
--- src/input/input.js
+++ src/input/input.js
@@ -1,21 +1,23 @@
 import { mount } from "../Component";
+import { addEvent } from "../factory";
 
 export default class InputComponent {
     static componentName = "Input";
-    static selector = ".a-input";
+    static selector = ".js-a-input";
 
-    events = ["focus", "blur"];
+    events = ["focus", "blur", "click"];
 
     constructor(node) {
         console.log("Register ", node);
     }
 
-    on(type, event) {
+    on(type, event, opts) {
         switch (type) {
             case "focus":
             case "blur":
+            case "click":
             default:
-                console.log("Listen to %s ", event);
+                console.log("Listen to %s %s", type, opts.delegate ? "(delegated)" : "", event);
                 break;
         }
     }
